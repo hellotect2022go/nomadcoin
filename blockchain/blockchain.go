b75dfbc56b5413b72479e60c9b8c7832387febed
hellotect2022 <hellotect2022@gmail.com>
2025-12-12 14:35:20 +0900

    [block] refactoring

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,23 +1,60 @@
 package blockchain
 
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
 type block struct {
-	data     string
-	hash     string
-	prevHash string
+	Data     string
+	Hash     string
+	PrevHash string
 }
 
 type blockChain struct {
-	blocks []block
+	blocks []*block
 }
 
 // Single Pattern 으로 만들기
-var b *blockChain
+var bc *blockChain
+var once sync.Once // 몇개의 채널이 있던 한번만 실행되도록 하기
+
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash) //16진수 : base16 문자열로 전환
+}
+
+func getLastHash() string {
+	totalBlocks := len(GetBlockChain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockChain().blocks[totalBlocks-1].Hash
+}
+
+func createBlock(data string) *block {
+	newBlock := block{data, "", getLastHash()}
+	newBlock.calculateHash()
+	return &newBlock
+}
+
+func (b *blockChain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
 
 func GetBlockChain() *blockChain {
-	if b == nil {
-		b = &blockChain{}
+	if bc == nil {
+		once.Do(func() {
+			bc = &blockChain{}
+			bc.AddBlock("Genesis Block")
+		})
 	}
-	return b
+	return bc
 }
 
 //
+
+func AllBlocks() []*block {
+	return GetBlockChain().blocks
+}
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/hellotect2022go/nomadcoin/blockchain"
+import (
+	"fmt"
+
+	"github.com/hellotect2022go/nomadcoin/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockChain()
+	chain.AddBlock("Secon Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+
+	for _, block := range blockchain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("PrevHash: %s\n", block.PrevHash)
+	}
+
 }
