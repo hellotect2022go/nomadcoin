182c27789593c20853d9b6b5a5282e9581c3e60a
hellotect2022 <hellotect2022@gmail.com>
2025-12-15 10:29:18 +0900

    json marshal

---
+++ explorer/explorer.go
@@ -0,0 +1,53 @@
+package explorer
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/hellotect2022go/nomadcoin/blockchain"
+)
+
+const (
+	port        string = ":4000"
+	templateDir string = "explorer/template/"
+)
+
+var templates *template.Template
+
+type HomeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	//tmpl := template.Must(template.ParseFiles("template/pages/home.gohtml"))
+	data := HomeData{"Home", blockchain.AllBlocks()}
+	//tmpl.Execute(w, data)
+	templates.ExecuteTemplate(w, "home", data)
+}
+
+func add(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("????", r.Method)
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(w, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		fmt.Println(data)
+		blockchain.GetBlockChain().AddBlock(data)
+		http.Redirect(w, r, "/", http.StatusMovedPermanently)
+	}
+
+}
+
+func Start() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil)) // 프로그램이 Exit(1) : {error 로 인해 종료시킴} 로그를 출력해줌
+}
--- main.go
+++ main.go
@@ -1,54 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 
-	"github.com/hellotect2022go/nomadcoin/blockchain"
+	"github.com/hellotect2022go/nomadcoin/utils"
 )
 
 const port string = ":4000"
-const templateDir string = "template/"
 
-var templates *template.Template
-
-type HomeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	//tmpl := template.Must(template.ParseFiles("template/pages/home.gohtml"))
-	data := HomeData{"Home", blockchain.AllBlocks()}
-	//tmpl.Execute(w, data)
-	templates.ExecuteTemplate(w, "home", data)
-}
-
-func add(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("????", r.Method)
-	switch r.Method {
-	case "GET":
-		templates.ExecuteTemplate(w, "add", nil)
-	case "POST":
-		r.ParseForm()
-		data := r.Form.Get("blockData")
-		fmt.Println(data)
-		blockchain.GetBlockChain().AddBlock(data)
-		http.Redirect(w, r, "/", http.StatusMovedPermanently)
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
 	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s\n", b)
 
 }
 
 func main() {
-	fmt.Println(blockchain.AllBlocks()[0].Data)
-	//tmpl := template.Must(template.ParseFiles("template/pages/home.gohtml"))
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
+	//explorer.Start()
+	http.HandleFunc("/", documentation)
 	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil)) // 프로그램이 Exit(1) : {error 로 인해 종료시킴} 로그를 출력해줌
+	log.Fatal(http.ListenAndServe(port, nil))
 
 }
